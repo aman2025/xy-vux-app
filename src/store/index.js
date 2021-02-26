ed40065cd45d0c3b73084d6117f3cc6910a6d88e
aman2025 <2247367564@qq.com>
2021-02-26 14:09:06 +0800

    nothing

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         menuActiveId: '999',
-        tabActiveIndex: 2
+        tabActiveIndex: 0
     },
     getters: {},
     mutations: {},
