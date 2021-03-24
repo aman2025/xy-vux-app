2515fb2c623db6856a9a397b40f5adb41f7edcd3
aman2025 <2247367564@qq.com>
2021-03-24 17:47:07 +0800

    绑定页面，剩retCode和error.code逻辑判断

--- src/views/Bind.vue
+++ src/views/Bind.vue
@@ -1,57 +1,64 @@
 <template>
     <div>
-        <div>绑定</div>
+        <group title="绑定平台账号">
+            <!-- 登录 -->
+            <x-input title="用户名" v-model="account.userCode"></x-input>
+            <x-input title="密码" type="password" v-model="account.password"></x-input>
+        </group>
+        <div @click="bindAccount" style="padding: 15px">
+            <x-button type="primary">绑定</x-button>
+        </div>
         <toast v-model="toastVisible" @on-hide="onHide">{{ toastText }}</toast>
     </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import { Toast } from 'vux';
+import { Toast, XInput, XButton, Group } from 'vux';
 import request from '../utils/request';
 
 export default {
     name: 'Bind',
     components: {
+        XInput,
+        XButton,
+        Group,
         Toast
     },
     data() {
         return {
+            account: { userCode: '', password: '' },
             toastVisible: false,
             toastText: '',
             toastCode: ''
         };
-    },
-    mounted() {
-        this.bindAccount();
     },
     methods: {
         // post保存
         bindAccount() {
             const url = '/mee/bind';
             const data = {
-                userCode: 'bms',
-                password: 'm0001'
+                userCode: this.account.userCode,
+                password: this.account.password
             };
-            const requestBind = (data) => request.post(url, data); //
+            const requestBind = (data) => request.post(url, data);
             requestBind(data).then((res) => {
                 this.toastVisible = true;
                 this.toastCode = res.retCode;
                 if (res.retCode == 0) {
                     this.toastText = '绑定成功';
                 } else {
-                    this.toastText = '绑定失败，授权码失效';
+                    this.toastText = res.error.message;
                 }
             });
         },
         // toast隐藏后触发
-        onHide(code) {
-            if (toastCode == 0) {
+        onHide() {
+            if (this.toastCode == 0) {
                 //成功后跳转
-                this.$router.go(-1);
+                this.$router.push('/');
             } else {
-                //失败后跳转
-                this.$router.go(-1);
+                //失败
             }
         }
     }
