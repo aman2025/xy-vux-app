151f4c7401da84b36858d0c03cde3ea94cf9f926
aman2025 <2247367564@qq.com>
2021-03-24 17:45:14 +0800

    app.vue还原

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,6 @@ import XyzCommonVue from 'xyz-common-vue';
 import Index from './views/Index';
 import Vue from 'vue';
 import request from './utils/request';
-import { AlertPlugin } from 'vux';
-Vue.use(AlertPlugin);
 
 export default {
     name: 'App',
@@ -21,34 +19,18 @@ export default {
     },
     components: {
         XyzCommonVue,
-        AlertPlugin,
         Index
     },
     mounted() {
-        if (!this.$route.meta.noauth) {
-            // todo: 非bind路由, 再router中处理，避免返回App.vue没有渲染
-            this.getUser();
-        }
+        this.getUser();
     },
     methods: {
         // 获取用户信息
         getUser() {
-            let _this = this;
             const url = '/mee/load-user';
-            const requestUser = () => request.post(url);
-            requestUser()
+            const requestMenu = () => request.post(url);
+            requestMenu()
                 .then((res) => {
-                    if (res.retCode != 0) {
-                        // 没绑定跳转到绑定页面
-                        this.$vux.alert.show({
-                            title: '用户未绑定平台账号',
-                            content: '',
-                            onShow() {},
-                            onHide() {
-                                _this.$router.push('/bind');
-                            }
-                        });
-                    }
                     console.log(res);
                 })
                 .catch(() => {});
