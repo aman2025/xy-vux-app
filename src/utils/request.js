21976a093eb66928835804b7808d85d77eb8d7a0
aman2025 <2247367564@qq.com>
2021-03-25 14:47:43 +0800

    error.code是其他情况弹出信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,5 @@
 /*
- * Copyright 1999-2018 Alibaba Group Holding Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+    封装axios
  */
 
 import axios from 'axios';
@@ -91,9 +79,14 @@ const request = () => {
         response => {
             const { error, retCode } = response.data;
 
-            if (retCode != 0 && error.code == 'BIZ.UN_AUTHN') {
-                //未登录
-                goLogin();
+            if (retCode != 0) {
+                if (error.code == 'BIZ.UN_AUTHN') {
+                    goLogin();
+                } else {
+                    alert(error.message);
+                }
+                closeLoading();
+
                 return Promise.reject(new Error(error.message));
             }
             closeLoading();
