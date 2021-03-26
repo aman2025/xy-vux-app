c1e6c23f2fbe6ada0ffaefba54811d4955dca7ad
aman2025 <2247367564@qq.com>
2021-03-26 09:52:26 +0800

    boardDetail路径

--- src/views/BoardDetail.vue
+++ src/views/BoardDetail.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <XHeader :title="title" headerType="line" :noright="true"></XHeader>
-        <div style="padding: 1rem">
-            <img src="../assets/detail01.png" />
-        </div>
+        <div style="padding: 1rem">详情页面</div>
     </div>
 </template>
 
