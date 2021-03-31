41d9e047176dd93d28d24c9b43888d5312af6014
aman2025 <2247367564@qq.com>
2021-03-31 10:33:36 +0800

    添加绑定失败页面

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,9 @@ import ManageApp from '../views/ManageApp.vue';
 import NoticeDetail from '../views/NoticeDetail.vue';
 import BoardDetail from '../views/BoardDetail.vue';
 import Bind from '../views/Bind.vue';
+import BindFailed from '../views/BindFailed.vue';
 import ObtainAuthCode from '../views/dingtalk/ObtainAuthCode.vue';
+import ValidLogin from '../views/ValidLogin.vue';
 
 Vue.use(Router);
 
@@ -14,6 +16,14 @@ const router = new Router({
     routes: [
         {
             path: '/',
+            name: 'validLogin',
+            component: ValidLogin,
+            meta: {
+                unRequireAuth: true
+            }
+        },
+        {
+            path: '/index',
             name: 'index',
             component: Index
         },
@@ -35,12 +45,26 @@ const router = new Router({
         {
             path: '/bind',
             name: 'bind',
-            component: Bind
+            component: Bind,
+            meta: {
+                unRequireAuth: true
+            }
+        },
+        {
+            path: '/bindFailed',
+            name: 'bindFailed',
+            component: BindFailed,
+            meta: {
+                unRequireAuth: true
+            }
         },
         {
             path: '/obtainAuthCode',
             name: 'obtainAuthCode',
-            component: ObtainAuthCode
+            component: ObtainAuthCode,
+            meta: {
+                unRequireAuth: true
+            }
         }
     ]
 });
---
+++ src/views/BindFailed.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <msg title="绑定失败" :description="description" :buttons="buttons" :icon="icon"></msg>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Msg, XButton } from 'vux';
+import request from '../utils/request';
+import { PageUtils } from '../utils/util';
+
+export default {
+    name: 'Bind',
+    components: {
+        Msg,
+        XButton
+    },
+    data() {
+        return {
+            description: '该账号已经被锁定或冻结,请联系管理员!',
+            icon: 'warn',
+            buttons: [
+                {
+                    type: 'primary',
+                    text: '绑定',
+                    onClick: this.toBind.bind(this)
+                },
+                {
+                    type: 'default',
+                    text: '首页',
+                    onClick: this.toHome.bind(this)
+                }
+            ]
+        };
+    },
+    methods: {
+        toBind() {
+            this.$router.push('/bind');
+        },
+        toHome() {
+            this.$router.push('/');
+        }
+    }
+};
+</script>
+<style lang="scss">
+</style>
